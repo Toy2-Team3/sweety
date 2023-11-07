30fc07777b3ad4aefcf00822556190207cff50a0
김다빈 <dabin@gimdabin-ui-MacBookAir.local>
2023-11-07 16:18:22 +0900

    Config: styled-components 설치

---
+++ src/components/community/Community-item-buttons/CommunityItemButtons.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CommunityItemButtons = () => {
+  return <div>CommunityItemButtons</div>;
+};
+
+export default CommunityItemButtons;
---
+++ src/components/community/community-create-item/CommunityCreateItem.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CommunityCreateItem = () => {
+  return <div>CommunityCreateItem</div>;
+};
+
+export default CommunityCreateItem;
