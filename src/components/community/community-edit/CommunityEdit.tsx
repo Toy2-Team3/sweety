1afca154bbd6f79298cbb4927a3fafb8cd3ca46d
김다빈 <991012dabin@gmail.com>
2023-11-07 17:41:35 +0900

    Feat: 커뮤니티 글 작성페이지 디자인 완료

---
+++ src/components/community/community-edit/CommunityEdit.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import CommunityItemButtons from '../Community-item-buttons/CommunityItemButtons';
+import styled from 'styled-components';
+
+const EditWrapper = styled.div`
+  width: 80%;
+  height: 80vh;
+  padding: 5rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  /* background-color: beige; */
+`;
+
+const InputForm = styled.form`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const InputWrapper = styled.div`
+  --width: 100%;
+  --padding: 0.5rem;
+
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  justify-content: left;
+  align-items: top;
+
+  label {
+    font-size: 2rem;
+    font-weight: bold;
+    white-space: nowrap;
+  }
+
+  input {
+    width: var(--width);
+    padding: var(--padding);
+
+    border-radius: 0.5rem;
+    border: 1px solid #949494;
+
+    font-size: 1.3rem;
+  }
+
+  textarea {
+    width: var(--width);
+    height: 10rem;
+    padding: var(--padding);
+
+    border-radius: 0.5rem;
+    border: 1px solid #949494;
+
+    font-size: 1rem;
+    font-weight: normal;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: right;
+`;
+
+const CommunityEdit = () => {
+  return (
+    <EditWrapper>
+      <InputForm>
+        <InputWrapper>
+          <label htmlFor="title">제목</label>
+          <input type="text" placeholder="제목을 입력하세요." id="title" />
+        </InputWrapper>
+        <InputWrapper>
+          <label htmlFor="content">내용</label>
+          <textarea placeholder="내용을 입력하세요." id="content" />
+        </InputWrapper>
+      </InputForm>
+      <ButtonWrapper>
+        <CommunityItemButtons />
+      </ButtonWrapper>
+    </EditWrapper>
+  );
+};
+
+export default CommunityEdit;
