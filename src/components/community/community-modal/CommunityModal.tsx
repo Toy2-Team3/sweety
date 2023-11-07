ea70a6b689d2d1096d93ee5d2c77f1cf0c9adb6e
김다빈 <991012dabin@gmail.com>
2023-11-07 17:11:19 +0900

    Feat:커뮤니티 모달 디자인 완료

--- src/components/community/community-modal/CommunityModal.tsx
+++ src/components/community/community-modal/CommunityModal.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import CommunityItemButtons from '../Community-item-buttons/CommunityItemButtons';
+import Close from '../../../assets/close.png';
+
+const Modal = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
 
 const ModalWrapper = styled.div`
   max-height: 20rem;
@@ -18,16 +24,31 @@ const ModalWrapper = styled.div`
   /* border: transparent; */
   border-radius: 1rem;
 
+  position: relative;
+
   div {
     display: flex;
     justify-content: right;
   }
 `;
 
+const CloseButton = styled.div`
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
+
+  img {
+    width: 20px;
+  }
+`;
+
 const CommunityModal = () => {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <Modal>
       <ModalWrapper>
+        <CloseButton>
+          <img src={Close} />
+        </CloseButton>
         <h1>매주 월요일 바이크 타실 분</h1>
         <p>
           안녕하세요, 바이크 소모임 000입니다! 저희 소모임은 매주 월요일 저녁
@@ -37,7 +58,7 @@ const CommunityModal = () => {
           <CommunityItemButtons />
         </div>
       </ModalWrapper>
-    </div>
+    </Modal>
   );
 };
 
