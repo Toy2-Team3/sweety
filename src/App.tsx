6b1ce215304ef2b0705d70c47a6f9e57a7ac60b5
김다빈 <991012dabin@gmail.com>
2023-11-07 16:58:31 +0900

    Feat: 버튼 디자인 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
+import Community from './components/community';
 
 function App() {
   return (
     <div className="App">
-      <button>asdfsdas</button>
-      <input type="text" />
+      <Community />
     </div>
   );
 }
--- src/components/community/index.tsx
+++ src/components/community/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import CommunityItem from './CommunityItem';
+// import CommunityItem from './community-item/CommunityItem';
+import CommunityModal from './community-modal/CommunityModal';
 
 const index = () => {
   return (
     <div>
       {/* map 돌리기 */}
+      {/* <CommunityItem />
       <CommunityItem />
-      <CommunityItem />
-      <CommunityItem />
+      <CommunityItem /> */}
+      <CommunityModal />
     </div>
   );
 };
